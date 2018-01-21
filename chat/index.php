33c7d6048c2903797566a1c23c788e5dbb320de3
TC <hello@kode.email>
2018-01-20 18:41:48 -0800

    initial commit - replace Converse with Inverse Jabber web client

---
+++ chat/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8"> 
+<meta name="description" content="Kode.IM - part of the KodeNet XMPP network serving Jabber accounts @kode.im">
+<meta name="keywords" content="kode.im, kodenet jabber, kode.im xmpp, kode jabber, kode xmpp, jabber chat, jabber accounts, jabber registration">
+<meta property="og:title" content="Kode.IM | XMPP" />
+<meta property="og:description" content="Kode.IM XMPP/Jabber network" />
+<meta property='og:image' content='images/kodenet-logo.png'/>
+
+<title>Kode.IM | Chat </title>
+
+<link rel="icon" href="https://kode.im/favicon.ico?v=2"/>
+
+<!-- Converse -->
+<link type="text/css" rel="stylesheet" media="screen" href="converse/css/inverse.css" />
+<script src="converse/dist/inverse.min.js"></script>
+<!-- Converse -->
+
+</head>
+<body> 
+
+<!-- Piwik respects your privacy -->
+<script type="text/javascript">
+  var _paq = _paq || [];
+  _paq.push(["setDomains", ["*.kode.im"]]);
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//stats.koderoot.net/";
+    _paq.push(['setTrackerUrl', u+'piwik.php']);
+    _paq.push(['setSiteId', 6]);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<noscript><p><img src="//stats.koderoot.net/piwik.php?idsite=6" style="border:0;" alt="" /></p></noscript>
+<!-- End Piwik Code -->
+
+<!-- Piwik Image Tracker-->
+<img src="https://stats.koderoot.net/piwik.php?idsite=6&rec=1" style="border:0" alt="" />
+<!-- End Piwik -->
+
+<script src="converse/mainchat.js"></script>
+</body>
+</html>
